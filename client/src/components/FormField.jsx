4fc01065aa1bbf36a4d45f0d1a4f1a9933cef5c1
Mamitiana <mamitiana@fulldigits.com>
2023-05-28 11:53:34 +0300

    51:02 should have a fake img showing... Error: form is not defined

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -19,6 +19,30 @@ const FormField = ({ labelName, type, name, placeholder, value, handleChange, is
                 </button>
             )}
         </div>
+        <input 
+            type={type}
+            id={name}
+            name={name}
+            placeholder={placeholder}
+            value={value}
+            onChange={handleChange}
+            required
+            className='bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3 border'
+        />
+
+        <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 justify-center items-center">
+            {form.photo ? (
+                <img 
+                    src={form.photo} 
+                    alt={form.prompt}
+                    className='w-full h-full object-contain' 
+                />
+            ):(
+                <img src={preview} alt="preview" 
+                    className='w-9/12 h-9/12 object-contain opacity-40'
+                />
+            )}
+        </div>
     </div>
   )
 }
