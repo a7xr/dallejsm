13c0f2308b3a4fd9305901cc3e56386314694277
Mamitiana <mamitiana@fulldigits.com>
2023-05-28 12:08:48 +0300

    Minor Fix

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -27,22 +27,9 @@ const FormField = ({ labelName, type, name, placeholder, value, handleChange, is
             value={value}
             onChange={handleChange}
             required
-            className='bg-gray-50 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3 border'
+            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
         />
 
-        <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 justify-center items-center">
-            {form.photo ? (
-                <img 
-                    src={form.photo} 
-                    alt={form.prompt}
-                    className='w-full h-full object-contain' 
-                />
-            ):(
-                <img src={preview} alt="preview" 
-                    className='w-9/12 h-9/12 object-contain opacity-40'
-                />
-            )}
-        </div>
     </div>
   )
 }
