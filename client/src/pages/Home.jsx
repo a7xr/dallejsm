c2c497b5128b734c7f8251db71a61be26f0c4658
Mamitiana <mamitiana@fulldigits.com>
2023-05-28 09:07:39 +0300

    33:19 writing the fn RenderedCards + calling it in the front

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,18 @@ import React, {useEffect, useState} from 'react'
 
 import {Loader, Card, FormField } from '../components';
 
+const RenderedCards = ({data, title}) => {
+    if(data?.length > 0) {
+        return data.map((post) => <Card key={post._id} {...post}/>)
+    }
+
+    return (
+        <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">
+            {title} title
+        </h2>
+    )
+}
+
 const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
@@ -35,6 +47,14 @@ const Home = () => {
                             Showing Result for <span className="text-[#222328]">{searchText} </span>
                         </h2>
                     )}
+                    <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3 ">
+                        {searchText ? (
+                            <RenderedCards 
+                                data="searchedResults"
+                                
+                            />
+                        ):()}
+                    </div>
                 </>
             )}
         </div>
