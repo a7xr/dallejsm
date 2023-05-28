78ae30264e98a154a63dc180ff98e540ecd0f98f
Mamitiana <mamitiana@fulldigits.com>
2023-05-28 08:19:13 +0300

    28:7 At /; Showing the loading circle + Some displaying basic text

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,10 +3,33 @@ import React, {useEffect, useState} from 'react'
 import {Loader, Card, FormField } from '../components';
 
 const Home = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [allPosts, setAllPosts] = useState(null);
   return (
-    <div>Home</div>
+    <section className="max-w-7x1 mx-auto">
+        <div>
+
+            <h1 className="font-extrabold text-[#222328] text-[32px]">
+                The Community showcase
+            </h1>
+            <p className="mt-2 text-[666e75] text-[16px] max-w[500px]">
+                Browse
+            </p>
+        </div>
+        <div className='mt-16'>
+            <FormField/>
+        </div>
+
+        <div className="mt-10">
+            {loading ? (
+                <div className="flex justify-center items-center">
+                    <Loader/>
+                </div>
+            ):(
+                <></>
+            )}
+        </div>
+    </section>
   )
 }
 
