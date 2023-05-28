b60f1d6933ee2caa3be18550b3ae9571bcd59b83
Mamitiana <mamitiana@fulldigits.com>
2023-05-28 08:10:56 +0300

    client/src/pages/Home.jsx is importing some comp, defining stateLoading + stateAllPosts

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,10 @@
 import React, {useEffect, useState} from 'react'
 
+import {Loader, Card, FormField } from '../components';
+
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
   return (
     <div>Home</div>
   )
