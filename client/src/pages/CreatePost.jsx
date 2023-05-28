291fd6968a0e6c6d670c203fae19636c8ab8619a
Mamitiana <mamitiana@fulldigits.com>
2023-05-28 12:54:59 +0300

    58:21 Clicking SurpriseMe_button changes the prompt

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -24,10 +24,11 @@ const CreatePost = () => {
   }
 
   const handleChange = (e) => {
-
+    setForm({ ...form, [e.target.name]: e.target.value })
   }
   const handleSurpriseMe = () => {
-
+    const randomPrompt = getRandomPrompt(form.prompt)
+    setForm({...form, prompt: randomPrompt})
   }
 
   return (
