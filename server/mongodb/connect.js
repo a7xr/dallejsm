023cbae4402899e77427cb89931d11be189ed333
Mamitiana <mamitiana@fulldigits.com>
2023-05-30 07:44:46 +0300

    1:6:0 Code to connect to MongoDB

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -1,4 +1,9 @@
 import mongoose from "mongoose";
 const connectDB = (ur) => {
   mongoose.set("strictQuery", true);
+
+  mongoose
+    .connect(url)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => console.log(err));
 };
