6303e6bb396a3f93f62f140327fd9878106feb4d
Mamitiana <mamitiana@fulldigits.com>
2023-05-30 07:34:46 +0300

    1:3:33 This is the helloworld with express, but still need to add more code to make it work !!

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
-const app = express()
+const app = express();
 app.use(cors());
-app.use(express.json({limit: '50mb'}))
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/", async (req, res) => {
+  res.send("Hello world");
+});
+
