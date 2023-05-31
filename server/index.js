dc173ba85b7adba455749e6189e6faa9be67398a
Mamitiana <mamitiana@fulldigits.com>
2023-05-31 07:31:03 +0300

    1:13:25 http_/api/v1/post >> ./routes/postRoutes.js, http_/api/v1/dalle >> ./routes/dalleRoutes.js

--- server/index.js
+++ server/index.js
@@ -3,12 +3,17 @@ import * as dotenv from "dotenv";
 import cors from "cors";
 
 import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
   res.send("Hello world");
@@ -21,7 +26,7 @@ const startServer = async () => {
       console.log("Server started on http://localhost:8080")
     );
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
 startServer();
