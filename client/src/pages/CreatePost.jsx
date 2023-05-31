bbcc60feae5e0ec68f6f7f12eeb819420b415ca6
Mamitiana <mamitiana@fulldigits.com>
2023-05-31 10:09:46 +0300

    1:38:43 preventDefault() is to stop the page to automatically reload, fn_handleSubmit is called when click button_shareWCommunity, then AFAIK (upload to cloudinary & save on DB)

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -38,8 +38,22 @@ const CreatePost = () => {
     }
   }
 
-  const handleSubmit = async() => {
-    
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+    if(form.prompt && form.photo){
+        setLoading(true)
+        try {
+            const response = await fetch('http://localhost:8080/api/v1/post',{
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(form)
+            })
+        } catch (error) {
+            
+        }
+    }
   }
 
   const handleChange = (e) => {
