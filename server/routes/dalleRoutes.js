300e9a7adbc91311a072f7354599e1fab88842cb
Mamitiana <mamitiana@fulldigits.com>
2023-05-31 07:53:33 +0300

    1:20:10 http_/api/v1/dalle is displaying "hello from dalle"

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -11,3 +11,9 @@ const router = express.Router();
 const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 
 const openai = new OpenAIApi(configuration);
+
+router.route("/").get((req, res) => {
+  res.send("Hello from dallE");
+});
+
+export default router;
