bcff28630296126d2fe9f37c3bd4b5ba6c50bdba
Mamitiana <mamitiana@fulldigits.com>
2023-05-31 08:41:14 +0300

    1:21:27 openai.createImage({...}, n=nbrOfImageToCreate, size=resolution, response_format=dunno

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -19,7 +19,12 @@ router.route("/").get((req, res) => {
 router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
-    const aiResponse = await openai.createImage()
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
   } catch (error) {}
 });
 
