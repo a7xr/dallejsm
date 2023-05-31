a851cb23e630954ffa1fd45ac08339cf3da1f930
Mamitiana <mamitiana@fulldigits.com>
2023-05-31 08:44:28 +0300

    1:22:56 Managing if there_s error generating while generating the image from AI

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -29,6 +29,7 @@ router.route("/").post(async (req, res) => {
     res.status(200).json({ photo: image });
   } catch (error) {
     console.log(error);
+    res.status(500).send(error?.response.data.error.message);
   }
 });
 
