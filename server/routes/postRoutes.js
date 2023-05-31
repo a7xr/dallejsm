4cdf975685c22f79b8d453ba41b08d414b4c50b9
Mamitiana <mamitiana@fulldigits.com>
2023-05-31 09:40:42 +0300

    1:34:16 putting into tryCatch the upload to cloudinary + inserting into DB + sending http_status

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -19,18 +19,22 @@ router.route("/").get(async (req, res) => {});
 
 // Create a Posts
 router.route("/").post(async (req, res) => {
-  const { name, prompt, photo } = req.body;
-  const photoUrl = cloudinary.uploader.upload(photo);
-  const newPost = await Post.create({
-    name,
-    prompt,
-    photo: photoUrl.url,
-  });
+  try {
+    const { name, prompt, photo } = req.body;
+    const photoUrl = cloudinary.uploader.upload(photo);
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    });
 
-  res.status(201).json({
-    success: true,
-    data: newPost,
-  });
+    res.status(201).json({
+      success: true,
+      data: newPost,
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error });
+  }
 });
 
 export default router;
